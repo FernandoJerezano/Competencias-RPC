19c2f205a4158fa3098c068475f2dc38b10ed17e
FernandoJerezano <fernando._jerezano@hotmail.com>
2017-09-03 19:30:39 -0500

    Create RPC 2017 08 - Prob E - Taking P.java

---
+++ RPC 2017 08 - Prob E - Taking P.java
@@ -0,0 +1,49 @@
+// Problema:	Talking P
+// Autor:		  FernandoJerezano
+// Fecha:		  02-Septiembre-2017
+import java.io.*;
+
+class talkingp{
+	
+	public static void main(String[]args)throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+		int n;
+		String s;
+		
+		n = Integer.parseInt(br.readLine());
+		char c;
+		
+		for(int i=0;i<n;i++){
+			
+			s = br.readLine();
+			
+			for(int j=0;j<s.length();j++){
+				c = s.charAt(j);
+				pw.print(c);
+				switch(c){
+					case 'a':
+						pw.print("pa");
+						break;
+					case 'e':
+						pw.print("pe");
+						break;
+					case 'i':
+						pw.print("pi");
+						break;
+					case 'o':
+						pw.print("po");
+						break;
+					case 'u':
+						pw.print("pu");
+						break;
+				}
+			}
+			pw.println();
+		}
+				
+		br.close();
+		pw.close();
+	}
+}
