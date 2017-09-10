9d66fd6153d11227648a608c9b87393d5c45d299
FernandoJerezano <fernando._jerezano@hotmail.com>
2017-09-10 10:25:38 -0500

    Create RPC 2017 08 - Prob E - Pythagorean triple.cpp

---
+++ RPC 2017 08 - Prob E - Pythagorean triple.cpp
@@ -0,0 +1,31 @@
+// Problema:	Pythagorean triple
+// Autor:     FernandoJerezano
+// Fecha:     09-Septiembre-2017
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int N,cont;
+	
+	while(scanf("%d",&N)){
+		if(!N)
+			break;
+		cont = 0;
+		for(int i=1;i<=N;i++){
+			for(int j=i;j<=N;j++){
+				for(int k=j;k<=N;k++){
+					//printf("%d %d = %d\n",i,j,k);
+					
+					if((i*i+j*j)==k*k){
+						cont++;
+					}
+				}
+			}
+		}
+		printf("%d\n",cont);
+	}
+	return 0;
+}
