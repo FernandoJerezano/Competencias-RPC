7e135a59343991913e03bd94450c91fd50b86aae
FernandoJerezano <fernando._jerezano@hotmail.com>
2017-10-21 19:50:07 -0500

    Competencia RPC 12 de 2017

---
+++ RPC 2017 12 - Prob D.cpp
@@ -0,0 +1,34 @@
+// Problema: Digital roots
+// Autor:    FernandoJerezano
+// Fecha:    21-Octubre-2018
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	unsigned long long int n,sum;
+	
+	//freopen("salida.txt","w",stdout);
+	
+	while(cin>>n){
+		
+		if(n==0)
+			break;
+		
+		while(n>=10){
+			sum = 0;
+			
+			while(n>0){
+				sum+=n%10;
+				n/=10;
+			}
+			n = sum;
+		}
+
+		printf("%d\n",(int)n);
+	}
+	
+	return 0;
+}
